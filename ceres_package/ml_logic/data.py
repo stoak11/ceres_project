1b48deec7fab0110f42df880317e3163d617d86b
Timothey Berl <timothey.berl@gmail.com>
2026-06-03 15:58:00 +0200

    Data(Meteo) : Done packaging meteo_cleaning and csv consolidation

--- ceres_package/ml_logic/data.py
+++ ceres_package/ml_logic/data.py
@@ -6,37 +6,56 @@ import re
 @simple_time_and_memory_tracker
 
 
-def clean_meteo_data(gcp_path) -> pd.DataFrame :
-    """ Import CSV locally """
-    # gcp_path = '/home/berlh/code/stoak11/ceres_project/raw_data/meteofrance/dept_59.csv'
-    df = pd.read_csv(gcp_path, sep=',', encoding='utf-8-sig')
+def clean_meteo_data(gcp_path) -> pd.DataFrame:
 
-    """ Drop useless columns """
-    cols_to_drop = [c for c in df.columns if c not in COLONNES_BLE]
-    df_lite = df.drop(columns=cols_to_drop)
-    print(f"Colonnes supprimées : {len(cols_to_drop)}")
-    print(f"Colonnes restantes  : {df_lite.shape[1]}")
+    dept_id = re.search(r'dept_(\w+)\.csv', gcp_path).group(1)
+    chunks_agg = []
+    """ Loop over one CSV """
+    for chunk in pd.read_csv(gcp_path, chunksize=50_000, sep=',', encoding='utf-8-sig'):
 
-    """ Convert Météo France timestamps """
-    df_lite["DATE"] = pd.to_datetime(df_lite["AAAAMMJJHH"].astype(str), format="%Y%m%d%H")
-    df_timeconvert = df_lite.drop(columns=["AAAAMMJJHH"])
+        """ Drop useless columns """
+        cols_to_drop = [c for c in chunk.columns if c not in COLONNES_BLE]
+        chunk = chunk.drop(columns=cols_to_drop)
 
-    print('🕘✅ Time Stamp Converted')
+        """ Convert Météo France timestamps """
+        chunk["DATE"] = pd.to_datetime(chunk["AAAAMMJJHH"].astype(str), format="%Y%m%d%H")
+        chunk = chunk.drop(columns=["AAAAMMJJHH"])
 
-    """ Rename columns with self-explanatory names """
-    df_col_renamed = df_timeconvert.rename(columns=RENAME_COLONNES_BLE)
+        """ Rename columns with self-explanatory names """
+        chunk = chunk.rename(columns=RENAME_COLONNES_BLE)
+        """ Add cleaned chunks to chunks list and delete current chunk from RAM """
+        chunks_agg.append(chunk)
+        del chunk
+
+    """ Concat cleaned chunks list and delete current chunks list from RAM """
+    df_dept = pd.concat(chunks_agg, ignore_index=True)
+    del chunks_agg
 
     """ Colonnes de mesure (hors identifiants station) """
-    mesure_cols = [c for c in df_col_renamed.columns if c not in ["id_station", "latitude", "longitude", "datetime", "dept_id"]]
+    mesure_cols = [c for c in df_dept.columns if c not in ["id_station", "latitude", "longitude", "datetime"]]
 
-    """" Aggregation by timestamp """
-    df_agg = df_col_renamed.groupby("datetime")[mesure_cols].mean().reset_index()
-
-    print(f"Shape avant agrégation : {df_col_renamed.shape}")
-    print(f"Shape après agrégation : {df_agg.shape}")
+    """" Aggregation by timestamp and delete pre-aggregation DataFrame from RAM """
+    df_agg = df_dept.groupby("datetime")[mesure_cols].mean(numeric_only=True).reset_index()
+    del df_dept
 
     """ Add column ['dept_id'] """
-    dept_id = re.search(r'dept_(\w+)\.csv', gcp_path).group(1)
     df_agg["dept_id"] = dept_id
 
+    print(f'✅ Département {dept_id} cleaned — {len(df_agg):,} lignes')
     return df_agg
+
+
+def consolidate_meteo_data(gcp_path, consolidated_file):
+    """ In Case of future update to consolidated all updated departments CSV """
+    first_write = True
+
+    for dept in range(21, 96):
+        dept_str = f'{dept:02d}'
+        gcp_path = f'/home/berlh/code/stoak11/ceres_project/raw_data/meteofrance/departements/dept_{dept_str}.csv'
+        df_temp = clean_meteo_data(gcp_path=gcp_path)
+        df_temp.to_csv(consolidated_file, mode='a', index=False, header = first_write)
+        first_write = False
+        print(f'✅ Département {dept_str} consolidated to full file')
+        del df_temp
+
+    print(f'\n✅ Terminé — {consolidated_file}')
